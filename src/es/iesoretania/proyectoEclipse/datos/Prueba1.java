522bfe9c7ad81bc66ba900ca17767d0d6ed437f8
Rocío Medina <rmedmor2904@g.educaand.es>
2024-10-30 09:14:29 +0100

    modificacion prueba 1 y prueba 2

--- src/es/iesoretania/proyectoEclipse/datos/Prueba1.java
+++ src/es/iesoretania/proyectoEclipse/datos/Prueba1.java
@@ -3,5 +3,8 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
 		System.out.println("Adiós mundo!)");
+		
+		System.out.println("Hola 1!");
+		System.out.println("Adiós 2!)");
 	}
 }
