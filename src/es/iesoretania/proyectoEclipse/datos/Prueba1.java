598fdaa83639c1c63ccfcf65c4d4f9c9644546f1
Rocío Medina <rmedmor2904@g.educaand.es>
2024-10-30 09:09:42 +0100

    version inicial

---
+++ src/es/iesoretania/proyectoEclipse/datos/Prueba1.java
@@ -0,0 +1,7 @@
+package es.iesoretania.proyectoEclipse.datos;
+public class Prueba1 {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+		System.out.println("Adiós mundo!)");
+	}
+}
