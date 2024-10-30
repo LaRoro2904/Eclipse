db14b2298dfb783475e539b2c74d06e4a858b8b8
Rocío Medina <rmedmor2904@g.educaand.es>
2024-10-30 09:16:20 +0100

    modificacion prueba 1 en master

--- src/es/iesoretania/proyectoEclipse/datos/Prueba1.java
+++ src/es/iesoretania/proyectoEclipse/datos/Prueba1.java
@@ -3,5 +3,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
 		System.out.println("Adiós mundo!)");
+			System.out.println("holis");
+			System.out.println("Adiós bye!)");
 	}
 }
